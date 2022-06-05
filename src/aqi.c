8b546bc82133d0ba97c657f8a297d4897a10296f
Luke Marzen <marzenlj@gmail.com>
2022-06-05 12:45:07 -0700

    created files

---
+++ src/aqi.c
@@ -0,0 +1,20 @@
+#ifndef __AQI_H__
+#define __AQI_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// see https://en.wikipedia.org/wiki/Air_quality_index for more details about
+// various aqi scales
+typedef enum aqi_scale {
+
+} aqi_scale_t;
+
+//int aqi_calc(const aqi_pollutant_type_t type, const float c, int *aqi);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
